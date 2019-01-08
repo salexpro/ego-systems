44092f3cdb641dbd8f0e63d4c084d677944c864d
Alexander <dev@salex.pro>
2019-01-08 14:00:18 +0200

    additional param in calc

--- src/js/components/_calc.js
+++ src/js/components/_calc.js
@@ -2,7 +2,10 @@ const numberWithSpaces = x => x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')
 
 const recalc = form => {
     const data = new FormData(form);
-    const sum = Array.from(data.values()).reduce((sum, item) => sum + Number(item), 0);
+    const sum = [...data.values()].reduce((sum, item) => {
+        const value = JSON.parse(item);
+        return sum + value.price + value.service;
+    }, 0);
     $('.item_calc_total').text(numberWithSpaces(sum) + ' ₽');
 }
 
